98d4a68af081fbca626556e17fd019cefd29bdb1
Anna Vasyukova <sucuub00@gmail.com>
2023-06-14 00:57:46 +0300

    Task_1

--- Base_JavaScript/Objects/Task_1/index.js
+++ Base_JavaScript/Objects/Task_1/index.js
@@ -1,1 +1,30 @@
-console.log("Hello");
+const users = [
+  {
+    username: 'David',
+    status: 'online',
+    lastActivity: 10,
+  },
+  {
+    username: 'Lucy',
+    status: 'offline',
+    lastActivity: 22,
+  },
+  {
+    username: 'Bob',
+    status: 'online',
+    lastActivity: 104,
+  },
+];
+
+const newArray = users.filter((item) => {
+  return item.status === 'online';
+});
+
+let onlineUsersName = newArray.map(item => {
+  return item.username
+});
+
+onlineUsersName = onlineUsersName.join(', ')
+console.log(onlineUsersName);
+
+alert(`Сейчас в онлайн следующие пользователи: ${onlineUsersName}`);
