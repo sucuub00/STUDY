4485d7880a3af2fb691a878741d84f268bf92926
Anna Vasyukova <sucuub00@gmail.com>
2023-07-28 17:33:52 +0300

    This(Task_1)

---
+++ Base_JavaScript/This/Task_1/index.js
@@ -0,0 +1,28 @@
+const student = {
+  stack: ['HTML'],
+  level: 1,
+  improveLevel: function () {
+    this.level++;
+    if (this.level === 2) {
+      this.stack.push('CSS');
+    }
+    if (this.level === 3) {
+      this.stack.push('JavaScript');
+    }
+    if (this.level === 4) {
+      this.stack.push('React');
+    }
+    if (this.level === 5) {
+      this.stack.push('NodeJS');
+    }
+    if (this.level > 5) {
+      alert(`Студент выучил все технологии!`);
+    }
+    return this;
+  },
+};
+
+console.log(student.improveLevel());
+console.log(student.improveLevel());
+console.log(student.improveLevel());
+console.log(student.improveLevel());
