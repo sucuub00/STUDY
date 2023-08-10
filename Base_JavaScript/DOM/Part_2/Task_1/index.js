676c75c833f19b824ba1d9a8f54be67c985c3910
Anna Vasyukova <sucuub00@gmail.com>
2023-08-10 22:31:06 +0300

    DOM(part2(task1))

---
+++ Base_JavaScript/DOM/Part_2/Task_1/index.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.create-task-block');
+const tasksList = document.querySelector('.tasks-list');
+
+const tasks = [];
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const { target } = event;
+  const { taskName: input } = target.elements;
+  const { value } = input;
+  tasks.push({
+    text: value,
+    id: Date.now(),
+  });
+  const task = myCreateElement(value, Date.now());
+  tasksList.insertAdjacentElement('beforeend', task);
+});
+
+function myCreateElement(text, id) {
+  const task = document.createElement('div');
+  task.id = id;
+  task.textContent = text;
+  return task;
+}
